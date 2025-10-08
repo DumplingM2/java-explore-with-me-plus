6902898a44a3cfa47a27d4a81d8f2a5175c1358c
Aleksei <andreev@almaz.spb.ru>
2025-10-08 09:29:12 +0300

    Изменена структура проекта, настроен Spring Cloud Eureka, Config Server, Stats Service, Main Service запускаются на случайном порту, Gateway - порт 8080.

--- infra/discovery-server/src/main/java/ru/practicum/explore/DiscoveryServerApplication.java
+++ infra/discovery-server/src/main/java/ru/practicum/explore/DiscoveryServerApplication.java
@@ -2,10 +2,12 @@ package ru.practicum.explore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class MainServer {
+@EnableEurekaServer
+public class DiscoveryServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MainServer.class, args);
+        SpringApplication.run(DiscoveryServerApplication.class, args);
     }
 }
